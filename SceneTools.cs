0c7d7c371d17af7a52a041b09d46bb0697994772
Maciejowski2006 <61437501+Maciejowski2006@users.noreply.github.com>
2020-04-10 20:19:44 +0200

    Scene Tools, Transform Tools

---
+++ SceneTools.cs
@@ -0,0 +1,29 @@
+﻿using UnityEditor;
+using UnityEngine;
+using UnityEditor.SceneManagement;
+
+public class SceneTools : EditorWindow
+{
+    [MenuItem("Compass Tools/Scene Tools")]
+    static void Init()
+    {
+        SceneTools window = (SceneTools)EditorWindow.GetWindow(typeof(SceneTools));
+        window.Show();
+    }
+
+    void OnGUI()
+    {
+        EditorGUILayout.BeginHorizontal();
+        if (GUILayout.Button("Main Menu") && EditorSceneManager.SaveCurrentModifiedScenesIfUserWantsTo())
+        {            
+            EditorSceneManager.OpenScene("Assets/Scenes/Main.unity", OpenSceneMode.Single);
+        }
+        if (GUILayout.Button("Game") && EditorSceneManager.SaveCurrentModifiedScenesIfUserWantsTo())
+        {
+            EditorSceneManager.OpenScene("Assets/Scenes/Game.unity", OpenSceneMode.Single);
+
+        }
+        EditorGUILayout.EndHorizontal();
+    }
+
+}
---
+++ TransformTool.cs
@@ -0,0 +1,91 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+public class TransformTools : EditorWindow
+{
+    [MenuItem("Compass Tools/Transform Tools")]
+    static void Init()
+    {
+        TransformTools window = (TransformTools)EditorWindow.GetWindow(typeof(TransformTools));
+        window.Show();
+        
+    }
+
+    void OnGUI()
+    {
+        EditorGUILayout.BeginHorizontal();
+        if (GUILayout.Button("Position"))
+        {
+            GameObject[] selectedObjects = Selection.gameObjects;
+
+            foreach (GameObject selected_object in selectedObjects)
+            {
+                Vector3 p_pos = Vector3.zero;
+                
+                if (selected_object.transform.parent != null)
+                {
+                    p_pos = selected_object.transform.parent.position;
+                }
+                
+                selected_object.transform.position = Vector3.zero + p_pos;
+            }
+        }
+        if (GUILayout.Button("Rotation"))
+        {
+            GameObject[] selectedObjects = Selection.gameObjects;
+
+            foreach (GameObject selected_object in selectedObjects)
+            {
+                Quaternion p_rot = Quaternion.identity;
+
+                if (selected_object.transform.parent != null)
+                {
+                    p_rot = selected_object.transform.parent.rotation;
+                }
+
+                selected_object.transform.rotation = Quaternion.identity * p_rot;
+            }
+
+        }
+        if (GUILayout.Button("Scale"))
+        {
+            GameObject[] selectedObjects = Selection.gameObjects;
+
+            foreach (GameObject selected_object in selectedObjects)
+            {
+                Vector3 p_scale = Vector3.one;
+
+                if (selected_object.transform.parent != null)
+                {
+                    p_scale = selected_object.transform.parent.localScale;
+                }
+
+                selected_object.transform.localScale = Vector3.one;
+            }
+        }
+        EditorGUILayout.EndHorizontal();
+        if (GUILayout.Button("Reset all"))
+        {
+            GameObject[] selectedObjects = Selection.gameObjects;
+
+            foreach (GameObject selected_object in selectedObjects)
+            {
+                Vector3 p_pos = Vector3.zero;
+                Quaternion p_rot = Quaternion.identity;
+                Vector3 p_scale = Vector3.one;
+
+                if (selected_object.transform.parent != null)
+                {
+                    p_pos = selected_object.transform.parent.position;
+                    p_rot = selected_object.transform.parent.rotation;
+                    p_scale = selected_object.transform.parent.localScale;
+                }
+
+                selected_object.transform.position = Vector3.zero + p_pos;
+                selected_object.transform.rotation = Quaternion.identity * p_rot;
+                selected_object.transform.localScale = Vector3.one;
+            }
+        }
+    }
+
+}
